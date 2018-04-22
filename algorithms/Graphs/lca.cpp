b21ca3b609b4d707cb2f8917498481004831c151
Miguel <miguel.sol.1997@outlook.com>
2018-04-22 17:03:31 -0500

    Added lca example

--- algorithms/Graphs/lca.cpp
+++ algorithms/Graphs/lca.cpp
@@ -1,11 +1,13 @@
 vector<int> tree[MAXN];
 int depth[MAXN];
 int parent[MAXN][level]; // level = log2(MAXN) + 1
+int sparse[MAXN][level]; // store something else, example heaviest edge from n to 2^i parent
 
 // pre-compute the depth for each node and their
 // first parent(2^0th parent)
 void dfs(int cur, int prev) {
     depth[cur] = depth[prev] + 1;
+	sparse[cur][0] = cost[cur -> prev(parent)];
     parent[cur][0] = prev;
     for (int i = 0; i < tree[cur].size(); i++) {
         if (tree[cur][i] != prev) dfs(tree[cur][i], cur);
@@ -17,7 +19,12 @@ void dfs(int cur, int prev) {
 void precomputeSparseMatrix(int n) {
     for (int i = 1; i < level; i++) {
         for (int node = 1; node <= n; node++) {
-            if (parent[node][i - 1] != -1) parent[node][i] = parent[parent[node][i - 1]][i - 1];
+            if (parent[node][i - 1] != -1) {
+				parent[node][i] = parent[parent[node][i - 1]][i - 1];
+				int a = sparse[node][i - 1];
+                int b = sparse[parent[node][i - 1]][i - 1];
+                sparse[node][i] = max(a, b); // heaviest to 2^i parent
+			}
         }
     }
 }
@@ -26,19 +33,24 @@ void precomputeSparseMatrix(int n) {
 int lca(int u, int v) {
     if (depth[v] < depth[u])
         swap(u, v);
-
+	int heaviest = 0;
     int diff = depth[v] - depth[u];
     for (int i = 0; i < level; i++)
-        if ((diff >> i) & 1)
+        if ((diff >> i) & 1) {
             v = parent[v][i];
+			heaviest = max(heaviest, sparse[v][i]);
+		}
 
     // now depth[u] == depth[v]
-    if (u == v) return u;
+    if (u == v) return u; // heaviest;
     for (int i = level - 1; i >= 0; i--)
         if (parent[u][i] != parent[v][i]) {
             u = parent[u][i];
             v = parent[v][i];
+			heaviest = max(heaviest, sparse[u][i]);
+            heaviest = max(heaviest, sparse[v][i]);
         }
+	// heaviest = max(heaviest, sparse[u][0], sparse[u][0]) if u or v != -1
     return parent[u][0];
 }
 
